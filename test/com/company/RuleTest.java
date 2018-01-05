309fac7b2ac8b8cac6cd54e8382e8ac11a056535
MohammadL <mzdhr@users.noreply.github.com>
2018-01-05 18:14:49 +0300

    topic 4 done

--- test/com/company/RuleTest.java
+++ test/com/company/RuleTest.java
@@ -10,27 +10,27 @@ import org.junit.rules.TestRule;
 import org.junit.rules.Timeout;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class RuleTest {
     // Fields
     private Calculator calculator;
 
-    // Rules
+    // Rule for Class
+    @ClassRule
+    public static TestRule timeout = Timeout.millis(5000);
+
+    // Rules for @Test Methods
     @Rule
-    public final TestRule globalTimeout = Timeout.millis(1005);
-    @Rule
-    public final ErrorCollector errorCollector = new ErrorCollector();
+    public final TestRule timeoutForMethods = Timeout.millis(1005);
     @Rule
     public final TestName testName = new TestName();
+    @Rule
+    public final ErrorCollector errorCollector = new ErrorCollector();
 
     // Custom Rule
     @Rule
     public final RuleWatchEye ruleWatchEye = new RuleWatchEye();
-
-    // Rule for class it self
-    @ClassRule
-    public static TestRule timeout = Timeout.millis(5000);
-
 
     @Before
     public void setUp() {
@@ -48,24 +48,34 @@ public class RuleTest {
         Thread.sleep(800);
     }
 
-
     @Test
     public void error_collector_test(){
         // Action
         int result01 = calculator.addition(2,2);
         int result02 = calculator.addition(3,3);
         int result03 = calculator.addition(4,4);
-
+        // Assertion
         errorCollector.checkThat(result01, is(3));  // wrong
         errorCollector.checkThat(result02, is(3));  // wrong
         errorCollector.checkThat(result03, is(3));  // wrong
     }
 
     @Test
-    public void method_name_test(){
-        System.out.println("This message printed from method -> " + testName.getMethodName());
+    public void no_error_collector_test(){
+        // Action
+        int result01 = calculator.addition(2,2);
+        int result02 = calculator.addition(3,3);
+        int result03 = calculator.addition(4,4);
+        // Assertion
+        assertThat(result01, is(3));  // wrong
+        assertThat(result02, is(3));  // wrong
+        assertThat(result03, is(3));  // wrong
     }
 
 
+    @Test
+    public void method_name_test(){
+        System.out.println("This message printed from method -> " + testName.getMethodName());
+    }
 
 }
--- test/com/company/RuleWatchEye.java
+++ test/com/company/RuleWatchEye.java
@@ -9,23 +9,29 @@ public class RuleWatchEye implements TestRule {
 
     @Override
     public Statement apply(Statement base, Description description) {
+
         return new Statement() {
+
             @Override
             public void evaluate() throws Throwable {
-
+                // ---------- Write Behavior ----------
                 long start = System.currentTimeMillis();
                 try {
-                    base.evaluate();
+                    // This code will run when each of @Test Methods runs.
+                    base.evaluate();    // Trigger for our @Test Methods code's.
                     System.out.println();
                     System.out.println("Method   ---> " + description.getMethodName());
                     System.out.println("In Class ---> " + description.getTestClass().getName());
                 } finally {
+                    // This code will run when each of @Test Methods is finish running.
                     System.out.println("Takes    ---> " + (System.currentTimeMillis() - start) + " Millisecond");
                 }
-
-
+                // ---------- End of Behavior ----------
             }
+
         };
+
     }
+
 
 }
